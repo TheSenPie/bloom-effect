223508e6de2e16c6a511374a859203c858f729e0
Smbo-2002 <smbo.voskanyan2002@gmail.com>
2021-01-03 14:11:46 +0400

    first commit

---
+++ desktop/src/com/rockbitegames/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rockbitegames.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rockbitegames.BloomEffect;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BloomEffect(), config);
+	}
+}
